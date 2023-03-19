ef6fb6277bb564a99c15aac7b2f050f905251d81
lizzzi111 <zinovyee@gmail.com>
2023-03-19 22:07:00 +0100

    feat: adding description code

--- Quantlet/Create_description/preprocessing_utils.py
+++ Quantlet/Create_description/preprocessing_utils.py
@@ -70,10 +70,17 @@ def load_script(code_script_path):
 
     with open(os.path.join(code_script_path), 'rb') as f:
         code = f.readlines()
-
-    code = [item for item in code if len(item)>0]
-    code = [line.decode() for line in code]
+    
+    new_code = []
+    for line in code: 
+        try: 
+            new_code.append(line.decode())
+        except:
+            pass
+    code = new_code    
+    #code = [line.decode() for line in code]
     code = [item.replace('\n', '') for item in code if item.endswith('\n')]
+    code = [item for item in code if len(item)>0]
     code = [f'{item}\n' for item in code]
     #code = ''.join(code)
     return code
@@ -83,7 +90,7 @@ def load_ipynb(code_script_path):
     """Load ipynb file and return a list of code snippets"""
 
     with open(code_script_path, 'rb') as f:
-        data = json.load(f)
+        data = json.load(f, strict=False)
     
     data = pd.json_normalize(data, record_path='cells')
 
@@ -121,21 +128,48 @@ def load_code(code_script_path, language='py'):
 class QuantletDataset(Dataset):
     def __init__(self, parsed_Qs_file):
         with open(parsed_Qs_file, 'rb') as f:
-            self.parsed_Qs_file = pickle.load(f)
+            loaded_dataset = pickle.load(f)
         
         # make sure the index is continuous    
-        self.parsed_Qs_file = self.parsed_Qs_file.reset_index(drop=True)
+        self.parsed_Qs_file = loaded_dataset
 
-        self.parsed_Qs_file.type_script = self.parsed_Qs_file.type_script.str.to_lower()
-        self.parsed_Qs_file = self.parsed_Qs_file[self.parsed_Qs_file.type_script.isin(['py', 'r', 'm', 'ipynb'])]
+        self.dw = 0
+
     def __len__(self):
         return len(self.parsed_Qs_file.script_name)
 
     def __getitem__(self, idx):
         subset = self.parsed_Qs_file.iloc[idx, :]
         code_script_path = os.path.join(subset.folder_name, subset.script_name)
-        code_snippet = load_code(code_script_path, subset.type_script.iloc)
+        try:
+            code_snippet = load_code(code_script_path, subset.type_script)
+        except:
+            self.dw += 1
+            code_snippet = 'empty'
         return code_snippet
     
     def show_df(self):
         return display(self.parsed_Qs_file) 
+    
+'''class QuantletDataloader(Dataloader):
+    def __init__(self, parsed_Qs_file):
+        self.dataset = QuantletDataset(parsed_Qs_file)
+        
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+        return self.dataset[idx]'''
+
+def set_seed(seed: int):
+    """
+    Helper function for reproducible behavior to set the seed in ``random``, ``numpy``, ``torch``
+
+    Args:
+        seed (:obj:`int`): The seed to set.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
