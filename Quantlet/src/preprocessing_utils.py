c0df0a79f9e5c9354c3fba751288a2b96cd5aa8d
lizzzi111 <zinovyee@gmail.com>
2023-07-02 16:41:02 +0000

    feat: addint the codes, chore: remove deprecated

--- Quantlet/src/preprocessing_utils.py
+++ Quantlet/src/preprocessing_utils.py
@@ -4,6 +4,7 @@ import pickle
 import pandas as pd
 from IPython.display import display
 from torch.utils.data import Dataset
+import numpy as np
 
 def check_if_import(code_snippet, language='py'):
     
@@ -175,7 +176,7 @@ def set_seed(seed: int):
     torch.cuda.manual_seed_all(seed)
     
     
-def parse_meta(row): 
+def parse_meta(row):
     row = row['metainfo_file']
     if row=='empty':
         return ['','','','']
@@ -204,3 +205,100 @@ def parse_meta(row):
         key = ''
     other = {k: row[k] for k in dict_keys if k not in dict_keys_used}
     return [name, desc, key, other]
+
+def batch_tokenize_preprocess(batch,
+                              tokenizer,
+                              max_source_length,
+                              max_target_length):
+
+    source = batch["input_sequence"]
+    target = batch["output_sequence"]
+
+    source_tokenized = tokenizer(
+        source,
+        padding="max_length",
+        truncation=True,
+        max_length=max_source_length
+    )
+
+    target_tokenized = tokenizer(
+        target,
+        padding="max_length",
+        truncation=True,
+        max_length=max_target_length
+    )
+
+    batch = {k: v for k, v in source_tokenized.items()}
+
+    # Ignore padding in the loss
+
+    batch["labels"] = [
+        [-100 if token == tokenizer.pad_token_id else token for token in l]
+        for l in target_tokenized["input_ids"]
+    ]
+
+    return batch
+    
+def postprocess_text(preds, labels):
+
+    preds = [pred.strip() for pred in preds]
+    labels = [label.strip() for label in labels]
+
+    # rougeLSum expects newline after each sentence
+    preds = ["\n".join(nltk.sent_tokenize(pred)) for pred in preds]
+    labels = ["\n".join(nltk.sent_tokenize(label)) for label in labels]
+
+    return preds, labels
+    
+def compute_metrics(eval_preds, metrics_list=['rouge', 'bleu']):
+
+    preds, labels = eval_preds
+
+    if isinstance(preds, tuple):
+        preds = preds[0]
+
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    # POST PROCESSING
+    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+
+    results_dict = {}
+    for m in metrics_list:
+        metric = evaluate.load(m)
+
+        if m=='bleu':
+            result = metric.compute(
+              predictions=decoded_preds, references=decoded_labels
+           )
+        elif m=='rouge':
+            result = metric.compute(
+                predictions=decoded_preds, references=decoded_labels, use_stemmer=True
+            )
+        result = {key: value for key, value in result.items() if key!='precisions'}
+        #result = {key: value * 100 for key, value in result.items() if 'rouge' in key}
+
+        prediction_lens = [
+            np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds
+        ]
+        result["gen_len"] = np.mean(prediction_lens)
+        result = {k: round(v, 4) for k, v in result.items()}
+        results_dict.update(result)
+    return results_dict
+     
+def generate_summary(test_samples, model):
+    inputs = tokenizer(
+        test_samples["input_sequence"],
+        padding="max_length",
+        truncation=True,
+        max_length=encoder_max_length,
+        return_tensors="pt",
+    )
+    input_ids = inputs.input_ids.to(model.device)
+    attention_mask = inputs.attention_mask.to(model.device)
+    outputs = model.generate(input_ids, attention_mask=attention_mask)
+    output_str = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    return outputs, output_str
